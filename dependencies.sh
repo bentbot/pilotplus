edafdcf29be4c00a8464441047e4e71458c9e470
Ubuntu <ubuntu@vbit.io>
2015-05-04 04:33:09 +0000

    Dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,8 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" |$
 apt-get -y update
 apt-get -y install mongodb-10gen
-
+mkdir /data
+mkdir /data/db
 wget http://download.redis.io/releases/redis-3.0.0.tar.gz
 tar xzf redis-3.0.0.tar.gz
 cd redis-3.0.0
